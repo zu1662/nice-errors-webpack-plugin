8cfc4b7e184d8d7bf6327fee06755f43b84dea70
huazite <zu1662@163.com>
2019-07-23 22:54:45 +0800

    修改输出顺序

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const ip = require('ip').address();
 
 class NiceErrorsWebpackPlugin extends FriendlyErrorsWebpackPlugin {
   displaySuccess(stats) {
+    super.displaySuccess(stats)
+    
     const port = stats.compilation.options.devServer.port
     console.log('你的应用程序在这里运行:');
     console.log('\r\n');
     console.log(` -local:   http://127.0.0.1:${port}`);
     console.log(` -newwork: http://${ip}:${port}`);
     console.log(`\r\n`);
-    
-    super.displaySuccess(stats)
   }
 }
 
