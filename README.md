dceb451c934d68baa6966e864eb3b5916d71ef6c
huazite <zu1662@163.com>
2019-07-23 22:34:16 +0800

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# nice-errors-webpack-plugin
+
+a custom webpack errors plugin base on friendly-errors-webpack-plugin.
+
+## Install
+
+### NPM
+
+```shell
+npm i nice-errors-webpack-plugin
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin') 
+// 获取局域网IP
+const ip = require('ip').address();
+
+class NiceErrorsWebpackPlugin extends FriendlyErrorsWebpackPlugin {
+  displaySuccess(stats) {
+    const port = stats.compilation.options.devServer.port
+    console.log('你的应用程序在这里运行:');
+    console.log('\r\n');
+    console.log(` -local:   http://127.0.0.1:${port}`);
+    console.log(` -newwork: http://${ip}:${port}`);
+    console.log(`\r\n`);
+    
+    super.displaySuccess(stats)
+  }
+}
+
+module.exports = NiceErrorsWebpackPlugin;
